7af64ca81d75ec5edb9b7234274652f8de3cd02c
ibrahim muhammad <ipcymuhammad1@gmail.com>
2024-11-07 14:47:01 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("flutter title"),
         ),
+        body: Center(child: Center(child: Text('Hello World!'))),
       ),
     );
   }
